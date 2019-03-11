83ea414d614ccc8cad10ce22bd5e7afe11e0ad12
Paul Allain <paul.allain@rosslyndatatech.com>
2019-03-11 16:08:45 +0000

    Flights retrieved in Container.

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -7,19 +7,30 @@ import Rotation from './Rotation.jsx';
 export default class Container extends Component {
   constructor(props) {
 		super(props);
-		this.state = { data: [] };
+		this.state = { aircrafts: [], flights: [] };
   }
   
 	componentWillMount() {
-		this.getData('https://infinite-dawn-93085.herokuapp.com/aircrafts');
+    this.getAircrafts();
+    this.getFlights();
   }
   
-  getData(url){
+  getAircrafts(){
     const xhr = new XMLHttpRequest();
-		xhr.open('get', url, true);
+		xhr.open('get', 'https://infinite-dawn-93085.herokuapp.com/aircrafts', true);
 		xhr.onload = () => {
-			const data = JSON.parse(xhr.responseText);
-			this.setState({ data: data.data });
+      const data = JSON.parse(xhr.responseText);
+			this.setState({ aircrafts: data.data });
+		};
+		xhr.send();
+  }
+
+  getFlights(){
+    const xhr = new XMLHttpRequest();
+		xhr.open('get', 'https://infinite-dawn-93085.herokuapp.com/flights', true);
+		xhr.onload = () => {
+      const data = JSON.parse(xhr.responseText);
+			this.setState({ flights: data.data });
 		};
 		xhr.send();
   }
@@ -27,9 +38,9 @@ export default class Container extends Component {
   render() {
     return (
       <div>
-        <Aircrafts aircrafts={this.state.data}/>
+        <Aircrafts aircrafts={this.state.aircrafts}/>
         <Rotation/>
-        <Flights url='https://infinite-dawn-93085.herokuapp.com/flights'/>
+        <Flights flights={this.state.flights}/>
       </div>
     );
   }
--- src/components/Flights.jsx
+++ src/components/Flights.jsx
@@ -4,26 +4,13 @@ import List from '@material-ui/core/List';
 import Flight from './Flight.jsx';
 
 export default class Flights extends Component {
-  constructor(props) {
-		super(props);
-		this.state = { data: [] };
-	}
-	componentWillMount() {
-		const xhr = new XMLHttpRequest();
-		xhr.open('get', this.props.url, true);
-		xhr.onload = () => {
-			const data = JSON.parse(xhr.responseText);
-			this.setState({ data: data.data });
-		};
-		xhr.send();
-	}
   render() {
     return (
       <div className="flights">
       Flights:
        <List>
        {
-         this.state.data.map(d => {
+         this.props.flights.map(d => {
           return (
             <Flight key={d.id} flight={d}></Flight>)
          })
