44f106b8bca6bd42ba80278f6448ac75efcd54ba
Paul Allain <paul.allain@rosslyndatatech.com>
2019-03-11 14:19:11 +0000

    Aircraft component.

---
+++ src/components/Aircraft.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+export default class Aircraft extends Component {
+  render() {
+    return (
+      <ListItem key={this.props.id} alignItems="flex-start">
+            <ListItemText
+              primary={this.props.aircraft.ident}
+              secondary={
+                <React.Fragment>
+                  {this.props.aircraft.type}
+                </React.Fragment>
+              }
+            />
+      </ListItem>
+    )
+  }
+}
