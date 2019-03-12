d930e245ed277662d31475a390beb438f47d4a24
Paul Allain <paul.allain@rosslyndatatech.com>
2019-03-12 10:34:22 +0000

    React notification

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import {NotificationContainer, NotificationManager} from 'react-notifications';
+import 'react-notifications/lib/notifications.css';
 
 import Aircrafts from './Aircrafts.jsx';
 import Flights from './Flights.jsx';
@@ -44,6 +46,7 @@ export default class Container extends Component {
                    flights: this.state.flights.filter(flight => flight.id !== data.id ),
                    rotations: this.state.rotations.filter(rotationObject => rotationObject.aircraft.ident !== this.state.activeAircraft.ident)
                    .concat({aircraft: rotationObject.aircraft, rotation: this.state.rotation.concat(data)})});
+    NotificationManager.success('Your flight ' + data.id + ' was added to the current rotation.', 'Flight ' + data.id + ' Added!');
   }
 
   removeRotationFlight(e, data) {
@@ -52,11 +55,13 @@ export default class Container extends Component {
                    flights: this.state.flights.concat(data),
                    rotations: this.state.rotations.filter(rotationObject => rotationObject.aircraft.ident !== this.state.activeAircraft.ident)
                    .concat({aircraft: rotationObject.aircraft, rotation: this.state.rotation.filter(rotationFlight => rotationFlight.id !== data.id )})});
+    NotificationManager.success('Your flight ' + data.id + ' was removed from the current rotation.', 'Flight ' + data.id + ' Removed!');
   }
 
   selectAircraft(e, data) {
     this.setState({activeAircraft: data,
                    rotation: this.state.rotations.find(rotationObject => rotationObject.aircraft.ident === data.ident).rotation});
+    NotificationManager.info('You are now looking at the rotation for the aircraft ' + data.ident + '.');
   }
 
   render() {
@@ -66,6 +71,7 @@ export default class Container extends Component {
         <Rotation activeAircraft={this.state.activeAircraft ? this.state.activeAircraft.ident : ""}
                    rotation={this.state.rotation} removeRotationFlight={this.removeRotationFlight.bind(this)}/>
         <Flights flights={this.state.flights} addRotationFlight={this.addRotationFlight.bind(this)}/>
+        <NotificationContainer/>
       </div>
     );
   }
