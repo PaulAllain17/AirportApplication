46b44c2da62af1609a52a2daa97452b33b85e7d9
Paul Allain <paul.allain17@gmail.com>
2019-03-12 00:17:26 +0000

    Switch from one rotation to another

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -7,7 +7,7 @@ import Rotation from './Rotation.jsx';
 export default class Container extends Component {
   constructor(props) {
 		super(props);
-		this.state = { aircrafts: [], flights: [], rotation: [], activeAircraft: null };
+		this.state = { aircrafts: [], flights: [], rotation: [], rotations: null, activeAircraft: null };
   }
   
 	componentWillMount() {
@@ -20,7 +20,10 @@ export default class Container extends Component {
 		xhr.open('get', 'https://infinite-dawn-93085.herokuapp.com/aircrafts', true);
 		xhr.onload = () => {
       const data = JSON.parse(xhr.responseText);
-			this.setState({ aircrafts: [{"ident":"GABCD","type":"A320","economySeats":186,"base":"EGKK"},{"ident":"HDBCD","type":"C820","economySeats":200,"base":"LDTT"}], activeAircraft: data.data[0] });
+      const rotationObject = [{"ident":"GABCD","type":"A320","economySeats":186,"base":"EGKK"},{"ident":"HDBCD","type":"C820","economySeats":200,"base":"LDTT"}]
+                             .map(aircraft => { return {aircraft: aircraft, rotation: []} });
+      this.setState({ aircrafts: [{"ident":"GABCD","type":"A320","economySeats":186,"base":"EGKK"},{"ident":"HDBCD","type":"C820","economySeats":200,"base":"LDTT"}],
+                      activeAircraft: data.data[0], rotations: rotationObject });
 		};
 		xhr.send();
   }
@@ -36,17 +39,24 @@ export default class Container extends Component {
   }
 
   addRotationFlight(e, data) {
+    const rotationObject = this.state.rotations.find(rotationObject => rotationObject.aircraft.ident === this.state.activeAircraft.ident);
     this.setState({rotation: this.state.rotation.concat(data), 
-                   flights: this.state.flights.filter(flight => flight.id != data.id )});
+                   flights: this.state.flights.filter(flight => flight.id !== data.id ),
+                   rotations: this.state.rotations.filter(rotationObject => rotationObject.aircraft.ident !== this.state.activeAircraft.ident)
+                   .concat({aircraft: rotationObject.aircraft, rotation: this.state.rotation.concat(data)})});
   }
 
   removeRotationFlight(e, data) {
-    this.setState({rotation: this.state.rotation.filter(rotationFlight => rotationFlight.id != data.id ), 
-                   flights: this.state.flights.concat(data)});
+    const rotationObject = this.state.rotations.find(rotationObject => rotationObject.aircraft.ident === this.state.activeAircraft.ident);
+    this.setState({rotation: this.state.rotation.filter(rotationFlight => rotationFlight.id !== data.id ), 
+                   flights: this.state.flights.concat(data),
+                   rotations: this.state.rotations.filter(rotationObject => rotationObject.aircraft.ident !== this.state.activeAircraft.ident)
+                   .concat({aircraft: rotationObject.aircraft, rotation: this.state.rotation.filter(rotationFlight => rotationFlight.id !== data.id )})});
   }
 
   selectAircraft(e, data) {
-    this.setState({activeAircraft: data});
+    this.setState({activeAircraft: data,
+                   rotation: this.state.rotations.find(rotationObject => rotationObject.aircraft.ident === data.ident).rotation});
   }
 
   render() {
